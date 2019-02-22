9e046e8962e4cc6c6cd9953526b288e78ce2ac11
Grzegorz Macialek <grzegorzmacialek@gmail.com>
2019-02-22 13:14:13 +0100

    Swagger UI

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# allegro_stats_api
+#### To run API
+###### With _Spring Boot Maven_ plugin
+`$ mvn spring-boot:run`
+###### With _Executable jar_
+`$ java -jar target/myapplication-0.0.1-SNAPSHOT.jar`
+
+#### Swagger UI:
+http://localhost:8080/swagger-ui.html
+
--- src/main/java/com/pagisoft/allegrostats/resources/PopularityDataResource.java
+++ src/main/java/com/pagisoft/allegrostats/resources/PopularityDataResource.java
@@ -2,29 +2,31 @@ package com.pagisoft.allegrostats.resources;
 
 import com.pagisoft.allegrostats.model.PopularityData;
 import com.pagisoft.allegrostats.service.PopularityDataService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/popularityData")
+@Api(value="popularity_data", description="Operations related to popularity data")
 public class PopularityDataResource {
 
     @Autowired
     private PopularityDataService popularityDataService;
 
     // http://localhost:8080/popularityData
-    @RequestMapping("")
+    @ApiOperation(value = "View a list of available popularity data", response = List.class)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<PopularityData> getAll() {
         return popularityDataService.getAllPopularityData();
     }
 
     // http://localhost:8080/popularityData/filterByCategoryId?categoryId=1
-    @RequestMapping("filterByCategoryId")
+    @ApiOperation(value = "View a list of available popularity data filtered by category id", response = List.class)
+    @RequestMapping(value = "filterByCategoryId", method = RequestMethod.GET)
     public List<PopularityData> findByCategoryId(@RequestParam(value="categoryId", required=true) String categoryId) {
         return popularityDataService.findByCategoryId(categoryId);
     }
